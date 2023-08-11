a9e3be99dd5786dde9515f8cabd7232abbd9a898
Anderson0x07 <andersonadrianorro@ufps.edu.co>
2023-08-11 01:10:04 -0500

    Cors solucionado en la puerta de enlace

--- demo/src/main/java/com/example/demo/repository/PruebaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Prueba;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PruebaRepository extends JpaRepository<Prueba, Integer> {
-}
---
+++ gateway/src/main/java/com/drogueria/gateway/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.drogueria.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod(HttpMethod.GET);
+        corsConfig.addAllowedMethod(HttpMethod.POST);
+        corsConfig.addAllowedMethod(HttpMethod.PUT);
+        corsConfig.addAllowedMethod(HttpMethod.DELETE);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
